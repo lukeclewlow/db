c3b3e4197e49e3d7054b8e7d9c96373a3c855b16
Luke Clewlow <luke.clewlow@gmail.com>
2015-01-31 22:49:54 +0000

    tidied up code a bit

--- lib/server.rb
+++ lib/server.rb
@@ -21,13 +21,8 @@ class DB < Sinatra::Base
 	# DataMapper.auto_migrate! 
 
 	get '/' do
-		# links = Link.all
-		# links.destroy
-		# tag = Tag.all
-		# tag.destroy
-		# fill_database
-		@links = Link.all	
-		@tags = Tag.all
+		@links = Link.all
+		@tags = Tag.all(:order => :text.asc)
 		erb :index
 	end
 
@@ -55,28 +50,5 @@ class DB < Sinatra::Base
 		erb :index
 	end
 
-	# get '/Folk' do
-	# 	@tags = Tag.all
-	# 	tag = Tag.all(:text => "Folk")
-	# 	@links = tag.links
-	# 	# erb :filtered
-	# end
-
-	# get '/Jungle' do
-	# 	@tags = Tag.all
-	# 	tag = Tag.all(:text => "Jungle")
-	# 	@links = tag.links
-	# 	# erb :filtered
-	# end
-
-
-	# def fill_database
-	# 	tags = "House ChillOut Jungle"
-	# 	tags = tags.split(" ").map { |tag| Tag.first_or_create(:text => tag)}
-	# 	Link.create(:url => '<iframe width="560" height="315" src="https://www.youtube.com/embed/t6bjqdll7DI" frameborder="0" allowfullscreen></iframe>', :title => "Rodriguez", :blog => "The man the legend")
-	# 	Link.create(:url => '<iframe width="100%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/179602402&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>', :title => "Max Cooper", :blog => "Super chilled", :tags => tags)
-	# 	Link.create(:url => '<iframe width="100%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/155628096&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>', :title => "DJ Vadim", :blog => "Sunny goodness", :tags => tags)
-	# 	Link.create(:url => '<iframe width="100%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/175495275&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>', :title => "Stivs", :blog => "Badman Ting")
-	# end
 
 end
