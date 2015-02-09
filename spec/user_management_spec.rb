1f2c098f463b052f93e6275bacc98760c46d086b
Luke Clewlow <luke.clewlow@gmail.com>
2015-02-09 10:17:12 +0000

    back to green

--- spec/user_management_spec.rb
+++ spec/user_management_spec.rb
@@ -9,12 +9,17 @@ scenario "when being a new user visiting the site" do
     expect(User.first.email).to eq("alice@example.com")
   end
 
+scenario "with a password that doesn't match" do
+  expect{ sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
+end
+
   def sign_up(email = "alice@example.com",
               password = "oranges!")
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email, :with => email
     fill_in :password, :with => password
+    fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign Up"
   end
 
